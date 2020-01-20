9794a5bdf375116f40e533aaa3baeacdd7224f82
Michael Stenta <mike@mstenta.net>
2020-01-20 15:24:26 -0500

    Allow hook_farm_info() items to specify an OAuth2 scope that allows access.

--- modules/farm/farm_api/farm_api.api.php
+++ modules/farm/farm_api/farm_api.api.php
@@ -30,8 +30,19 @@
 function hook_farm_info() {
   global $base_url, $conf;
   $info = array(
+
+    // Info items can be added simply:
     'name' => $conf['site_name'],
     'url' => $base_url,
+
+    // Or, they can be arrays with `info` and `scope` keys. The `info` is what
+    // will be included in the farmOS API info array. The `scope` is an OAuth2
+    // scope that will be checked for access. This allows some information to
+    // be available to OAuth2-authenticated services without a full user log in.
+    'foo' => array(
+      'info' => 'bar',
+      'scope' => 'access_foo',
+    ),
   );
   return $info;
 }
