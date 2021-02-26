c63c25cda42da2f4a7195d072c2ea417adb920c0
Michael Stenta <mike@mstenta.net>
2021-02-25 22:44:09 -0500

    Initial farm_ui_guide module for incorporating the farmOS user guide.

---
+++ modules/ui/guide/src/Controller/GuideController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\farm_ui_guide\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Guide controller.
+ *
+ * @ingroup farm
+ */
+class GuideController extends ControllerBase {
+
+  /**
+   * Builds the farmOS user guide page.
+   *
+   * @return array
+   *   Render array.
+   */
+  public function guide() {
+
+    // Start a build array.
+    $build = [];
+
+    // Return the build array.
+    return $build;
+  }
+
+}
