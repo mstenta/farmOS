70ac02a055f3bc4373fbf8f3e3833e3122fbc1fd
Michael Stenta <mike@mstenta.net>
2023-08-17 11:17:21 -0400

    Add basic CSV importers for assets (with kernel tests).

---
+++ modules/core/import/modules/csv/src/Plugin/Derivative/CsvImportMigrationAsset.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\farm_import_csv\Plugin\Derivative;
+
+/**
+ * Asset CSV import migration derivatives.
+ */
+class CsvImportMigrationAsset extends CsvImportMigrationBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected string $entityType = 'asset';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterProcessMapping(array &$mapping, string $bundle): void {
+
+    // Set the asset type.
+    $mapping['type'] = [
+      'plugin' => 'default_value',
+      'default_value' => $bundle,
+    ];
+  }
+
+}
