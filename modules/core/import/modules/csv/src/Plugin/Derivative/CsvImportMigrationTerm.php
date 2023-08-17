d4c260e60905024ee47247cf89570c49cd916a5a
Michael Stenta <mike@mstenta.net>
2023-08-17 11:17:43 -0400

    Issue #2931181: Add CSV importers for taxonomy terms (with kernel tests).

---
+++ modules/core/import/modules/csv/src/Plugin/Derivative/CsvImportMigrationTerm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\farm_import_csv\Plugin\Derivative;
+
+/**
+ * Term CSV import migration derivatives.
+ */
+class CsvImportMigrationTerm extends CsvImportMigrationBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected string $entityType = 'taxonomy_term';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterProcessMapping(array &$mapping, string $bundle): void {
+
+    // Set the vocabulary.
+    $mapping['vid'] = [
+      'plugin' => 'default_value',
+      'default_value' => $bundle,
+    ];
+  }
+
+}
