d4bb5d4bfd67dd44f89586ad9de0cfd6acf0df6e
Michael Stenta <mike@mstenta.net>
2023-09-20 11:11:24 -0400

    Split access checking logic out to a reusable service class.

---
+++ modules/core/import/modules/csv/src/Access/CsvImportMigrationAccess.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\farm_import_csv\Access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\migrate\Plugin\MigrationPluginManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class CsvImportMigrationAccess {
+
+  /**
+   * The migration plugin manager.
+   *
+   * @var \Drupal\migrate\Plugin\MigrationPluginManager
+   */
+  protected $pluginManagerMigration;
+
+  /**
+   * MigrateSourceUiForm constructor.
+   *
+   * @param \Drupal\migrate\Plugin\MigrationPluginManager $plugin_manager_migration
+   *   The migration plugin manager.
+   */
+  public function __construct(MigrationPluginManager $plugin_manager_migration) {
+    $this->pluginManagerMigration = $plugin_manager_migration;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('plugin.manager.migration'),
+    );
+  }
+
+  /**
+   * Checks access to a migration.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The user account to check access for.
+   * @param string $migration_id
+   *    The migration ID.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   An access result.
+   */
+  public function access(AccountInterface $account, string $migration_id) {
+
+    // Load the migration definition.
+    $definition = $this->pluginManagerMigration->getDefinition($migration_id);
+
+    // If the source plugin is csv_file, and it is in the farm_import_csv
+    // migration group, check access based on third party settings.
+    if ($definition['source']['plugin'] == 'csv_file' && $definition['migration_group'] == 'farm_import_csv') {
+      $permissions = [];
+      if (!empty($definition['third_party_settings']['farm_import_csv']['access']['permissions'])) {
+        $permissions = $definition['third_party_settings']['farm_import_csv']['access']['permissions'];
+      }
+      return AccessResult::allowedIfHasPermissions($account, $permissions);
+    }
+
+    // Otherwise, deny access.
+    return AccessResult::forbidden();
+  }
+
+}
