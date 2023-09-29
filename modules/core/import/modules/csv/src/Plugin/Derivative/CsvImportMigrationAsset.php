1c252b4a1bcc6676822b14e1050be1d908046767
Michael Stenta <mike@mstenta.net>
2023-09-29 14:20:17 -0400

    add id tag support

--- modules/core/import/modules/csv/src/Plugin/Derivative/CsvImportMigrationAsset.php
+++ modules/core/import/modules/csv/src/Plugin/Derivative/CsvImportMigrationAsset.php
@@ -32,6 +32,42 @@ class CsvImportMigrationAsset extends CsvImportMigrationBase {
       'plugin' => 'default_value',
       'default_value' => $bundle,
     ];
+
+    // ID tags.
+    $mapping['id_tag/0/id'] = [
+      'plugin' => 'get',
+      'source' => 'id tag',
+    ];
+    $mapping['id_tag/0/type'] = [
+      'plugin' => 'get',
+      'source' => 'id tag type',
+    ];
+    $mapping['id_tag/0/location'] = [
+      'plugin' => 'get',
+      'source' => 'id tag location',
+    ];
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterColumnDescriptions(array &$columns, string $bundle): void {
+    parent::alterColumnDescriptions($columns, $bundle);
+
+    // Describe the ID tag columns.
+    $columns[] = [
+      'name' => 'id tag',
+      'description' => 'ID tag.',
+    ];
+    $columns[] = [
+      'name' => 'id tag type',
+      'description' => 'The type of ID tag. Allowed values: ' . implode(', ', array_keys(farm_id_tag_type_options($bundle))) . '.',
+    ];
+    $columns[] = [
+      'name' => 'id tag location',
+      'description' => 'Location of the ID tag.',
+    ];
   }
 
 }
