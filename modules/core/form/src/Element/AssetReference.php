110c516957fc44987f892d9d9c9825d8ebebd27c
Michael Stenta <mike@mstenta.net>
2023-10-19 14:34:40 -0400

    wip: refactor asset_reference to be based on checkboxes
    reviewed this with @paul121 and ultimately it probably doesn't make
    sense to take this approach. however there may be some valuable things
    in this that could be preserved, so I'm pushing it as a separate commit
    for future reference.

--- modules/core/form/src/Element/AssetReference.php
+++ modules/core/form/src/Element/AssetReference.php
@@ -3,102 +3,132 @@
 namespace Drupal\farm_form\Element;
 
 use Drupal\asset\Entity\AssetInterface;
-use Drupal\Component\Utility\Html;
-use Drupal\Component\Utility\NestedArray;
-use Drupal\Core\Entity\Element\EntityAutocomplete;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Render\Element\FormElement;
+use Drupal\Core\Render\Element\Checkboxes;
 
 /**
  * Form element for referencing assets.
  *
+ * This renders a checkboxes form element for selecting asset entities. Below
+ * the checkboxes is an autocomplete search textfield which adds assets to the
+ * list of options.
+ *
+ * The #default_value accepts an array of Asset entities.
+ *
  * @FormElement("asset_reference")
  */
-class AssetReference extends FormElement {
+class AssetReference extends Checkboxes {
 
   /**
    * {@inheritdoc}
    */
   public function getInfo() {
+    $info = parent::getInfo();
     $class = static::class;
-    return [
-      '#process' => [
-        [$class, 'processElement'],
+
+    // Apply default form element properties.
+    $info['#selection_settings'] = [
+      'sort' => [
+        'field' => 'status',
+        'direction' => 'ASC',
       ],
     ];
-  }
+    $info['#placeholder_text'] = t('Search assets');
 
-  public static function processElement(&$element, FormStateInterface $form_state, &$complete_form) {
-    $element['#tree'] = TRUE;
+    // Process the element with our method first.
+    array_unshift($info['#process'], [$class, 'processAssetReference']);
 
-    // Create a wrapper with a unique ID.
-    $id_prefix = implode('-', $element['#parents']);
-    $wrapper_id = Html::getUniqueId($id_prefix . '-ajax-wrapper');
-    $element['#prefix'] = '<div id="' . $wrapper_id . '">';
-    $element['#suffix'] = '</div>';
-
-    // Add asset from search field input to default values.
-    $search_value = $form_state->getValue(array_merge($element['#array_parents'], ['search']));
-    if (!empty($search_value)) {
-      $asset_id = EntityAutocomplete::extractEntityIdFromAutocompleteInput($search_value);
-      if (!empty($asset_id)) {
-        $element['#default_value'][] = $asset_id;
-      }
-    }
-
-    // Build a list of checkbox options from default value.
-    $asset_options = [];
-    if (!empty($element['#default_value']) && is_array($element['#default_value'])) {
-      $asset_ids = [];
-      foreach ($element['#default_value'] as $value) {
-        if (is_numeric($value)) {
-          $asset_ids[] = $value;
-        }
-      }
-      $assets = \Drupal::service('entity_type.manager')->getStorage('asset')->loadMultiple($asset_ids);
-      $asset_options[] = array_map(function (AssetInterface $asset) {
-        return $asset->label();
-      }, $assets);
-    }
-
-    // Add checkboxes element, with all options checked.
-    $element['checkboxes'] = [
-      '#type' => 'checkboxes',
-      '#title' => $element['#title'],
-      '#description' => $element['#description'],
-      '#options' => $asset_options,
-      '#default_value' => $element['#default_value'],
-    ];
-
-    # Entity autocomplete search.
-    $element['search'] = [
-      '#type' => 'entity_autocomplete',
-      '#target_type' => 'asset',
-      '#selection_settings' => [
-        'sort' => [
-          'field' => 'status',
-          'direction' => 'ASC',
-        ],
-      ],
-      '#ajax' => [
-        'callback' => [get_called_class(), 'ajaxRefresh'],
-        'wrapper' => $wrapper_id,
-        'event' => 'autocompleteclose change',
-      ],
-    ];
-
-    // Return the element.
-    return $element;
+    return $info;
   }
 
   /**
-   * Ajax callback.
+   * {@inheritdoc}
    */
-  public static function ajaxRefresh(array $form, FormStateInterface $form_state) {
-    $triggering_element = $form_state->getTriggeringElement();
-    $parents = $triggering_element['#array_parents'];
-    array_pop($parents);
-    $element = NestedArray::getValue($form, $parents);
+  public static function valueCallback(&$element, $input, FormStateInterface $form_state) {
+
+    // Process the #default_value property.
+    if ($input === FALSE && isset($element['#default_value'])) {
+
+      // Convert the default value into an array.
+      if (!empty($element['#default_value']) && !is_array($element['#default_value'])) {
+        $element['#default_value'] = [$element['#default_value']];
+      }
+
+      // Convert to asset IDs.
+      foreach ($element['#default_value'] as $key => $asset) {
+
+        // Make sure default values are asset objects.
+        if (!($asset instanceof AssetInterface)) {
+          throw new \InvalidArgumentException('The #default_value property has to be an asset object or an array of asset objects.');
+        }
+
+        // Save the asset ID.
+        $element['#default_value'][$key] = $asset->id();
+      }
+    }
+
+    // If user input was provided, unset the search value.
+    if ($input !== FALSE && is_array($input)) {
+      unset($input['search']);
+    }
+
+    // Delegate to parent method.
+    return parent::valueCallback($element, $input, $form_state);
+  }
+
+  /**
+   * Process asset reference form element.
+   *
+   * @param array $element
+   *   The form element to process.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   * @param array $complete_form
+   *   The complete form structure.
+   *
+   * @return array
+   *   The form element.
+   */
+  public static function processAssetReference(array &$element, FormStateInterface $form_state, array &$complete_form) {
+
+    // Add an entity_autocomplete search box.
+    $element['search'] = [
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'asset',
+      '#attributes' => [
+        'placeholder' => $element['#placeholder_text'],
+      ],
+      '#selection_settings' => $element['#selection_settings'],
+    ];
+
+    // Load selected assets from #value or #default_value, if available.
+    // Convert to a list of asset IDs for internal processing.
+    /** @var \Drupal\asset\Entity\AssetInterface $assets */
+    $assets = [];
+    if (!empty($element['#default_value'])) {
+      foreach ($element['#default_value'] as $id) {
+        $element['#default_value'][$id] = $id;
+        $assets[] = \Drupal::entityTypeManager()->getStorage('asset')->load($id);
+      }
+    }
+    if (!empty($element['#value'])) {
+      foreach ($element['#value'] as $id) {
+        $element['#default_value'][$id] = $id;
+        $assets[] = \Drupal::entityTypeManager()->getStorage('asset')->load($id);
+      }
+    }
+
+    // Build checkbox options.
+    // Use the special 'view label' access check, since some entities allow the
+    // label to be viewed, even if the entity is not allowed to be viewed.
+    $element['#options'] = [];
+    if (!empty($assets)) {
+      foreach ($assets as $asset) {
+        $element['#options'][$asset->id()] = ($asset->access('view label')) ? $asset->label() : t('- Restricted access -');
+      }
+    }
+
+    // Return the element.
     return $element;
   }
 
