8294416d1dc6e30325a93587288435d2dcee5f41
Michael Stenta <mike@mstenta.net>
2024-03-27 06:48:11 -0400

    wip

---
+++ modules/report/geometry/src/Plugin/views/filter/GeometryFilter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\farm_report_geometry\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\filter\FilterPluginBase;
+
+/**
+ * A geometry filter.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("geometry")
+ */
+class GeometryFilter extends FilterPluginBase {
+
+
+
+}
