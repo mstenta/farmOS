9fdeac33ea568b6237b382a45ea71e25b29256eb
Paul Weidner <paul.weidner@gmail.com>
2024-07-26 17:12:27 -0700

    Add quantity normalizer to include log data in CSV exports.

---
+++ modules/core/log/modules/quantity/src/Normalizer/QuantityNormalizer.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\farm_log_quantity\Normalizer;
+
+use Drupal\farm_csv\Normalizer\ContentEntityNormalizer;
+use Drupal\quantity\Entity\QuantityInterface;
+
+/**
+ * Normalizes quantity entities for CSV exports.
+ */
+class QuantityNormalizer extends ContentEntityNormalizer {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function normalize($entity, $format = NULL, array $context = []): array|string|int|float|bool|\ArrayObject|NULL {
+    $data = parent::normalize($entity, $format, $context);
+
+    // Query the log associated with the quantity.
+    /** @var \Drupal\log\Entity\LogInterface[] $logs */
+    $logs = $this->entityTypeManager->getStorage('log')->loadByProperties([
+      'quantity' => $entity->id(),
+    ]);
+
+    // Prepend log data to the quantity data for normalization.
+    if ($log = reset($logs)) {
+      $data = [
+        'log_id' => $log->id(),
+        'log_status' => $log->get('status')->value,
+        'log_timestamp' => $this->serializer->normalize($log->get('timestamp')->first(), $format, $context),
+        'log_type' => $log->bundle(),
+        'log_name' => $log->getName(),
+      ] + $data;
+    }
+    return $data;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function supportsNormalization($data, string $format = NULL, array $context = []): bool {
+    return $data instanceof QuantityInterface && $format == static::FORMAT;
+  }
+
+}
